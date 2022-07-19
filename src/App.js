0533babffa9fe10c7dd5dde2b200f3ac73d3007f
Shubham Gupta <shubham.developer16@gmail.com>
2022-07-19 22:24:53 +0530

    Code Reduced

--- src/App.js
+++ src/App.js
@@ -6,44 +6,37 @@ import Services from './components/homepage/Services';
 import Testimonials from './components/homepage/Testimonials';
 import Footer from "./components/homepage/Footer";
 import ContactUs from "./components/homepage/ContactUs";
-import LoginSignup from "./components/LoginSignup";
 
-import { AuthProvider } from './contexts/AuthContext';
 import Loading from './components/Loading';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import BookingPage from './components/BookingPage';
-import { FirestoreProvider } from './contexts/FirestoreContext';
 
 
 function App() {
   const name = 'QT-parlour'
-  const [isAuthPopupVisible, setAuthPopupVisible] = useState(false)
   const [isLoading, setLoading] = useState(false)
 
 
   // alert(location.pathname)
 
   return (
-    <AuthProvider>
-      <FirestoreProvider>
-        <BrowserRouter>
-          <Navbar title={name} onLoading={setLoading} onSignInClicked={() => { setAuthPopupVisible(true) }} />
-          <Routes>
-            <Route exact path='/' element={<>
+    <>
+      <BrowserRouter>
+        <Navbar title={name} onLoading={setLoading} onSignInClicked={() => { }} />
+        <Routes>
+          <Route exact path='/' element={
+            <>
               <Header title={name} />
               <About />
               <Services items={[1, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,]} />
               <Testimonials />
               <ContactUs />
-            </>} />
-            <Route path='/booking' element={<BookingPage />} />
-          </Routes>
-        </BrowserRouter>
-        <Footer />
-        <LoginSignup visible={isAuthPopupVisible} onLoading={setLoading} onClose={() => { setAuthPopupVisible(false) }} />
-        <Loading visible={isLoading} />
-      </FirestoreProvider>
-    </AuthProvider>
+            </>
+          } />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+      <Loading visible={isLoading} />
+    </>
   );
 }
 
