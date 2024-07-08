0af112ecf6137a7f9b8025995dc3b120e9bb3010
Shubham Gupta <shubham.developer16@gmail.com>
2024-07-09 01:09:19 +0530

    Update meta description in index.html and add social media link in content.json

--- src/components/homepage/Header.js
+++ src/components/homepage/Header.js
@@ -15,9 +15,11 @@ export default function Header() {
                         <p className="header-body" data-aos="fade-up" data-aos-delay='500'>{content.headerBody}</p>
                         <div className="footer-timing social-links">
                             {content.socialLinks.map(function (object, i) {
-                                return <a href={object.href} className={object.className} data-aos="fade-up" data-aos-delay={i * 50 + 600} target='_blank' rel="noopener noreferrer">
-                                    <i className={object.iconClass} aria-hidden="true"></i>
-                                </a>
+                                return <span data-aos="fade-up" data-aos-delay={i * 50 + 600} >
+                                    <a href={object.href} className={object.className} target='_blank' rel="noopener noreferrer">
+                                        <i className={object.iconClass}></i>
+                                    </a>
+                                </span>
                             })}
                         </div>
                         <br />
@@ -27,10 +29,10 @@ export default function Header() {
                             <a href={content.headerButton1.href} target='_blank' rel="noopener noreferrer" data-aos="fade-up" data-aos-delay='900' className="b-btn hard">{content.headerButton1.text}</a>
                             <a href={content.headerButton2.href} target='_blank' rel="noopener noreferrer" data-aos="fade-up" data-aos-delay='1000' style={{ marginLeft: '15px' }} className="b-btn hard inverse">{content.headerButton2.text}</a>
                         </div>
-                        
+
                     </div>
                     {/* <div className="header-image" data-aos="fade-up"> */}
-                        {/* <img src={pic} alt="Header" /> */}
+                    {/* <img src={pic} alt="Header" /> */}
                     {/* </div> */}
                 </div>
             </div>
